92c4e6a117f447f490b3bd3ba22d68a79c642da2
AlcidesTheGurj <93118250+AlcidesTheGurj@users.noreply.github.com>
2022-03-17 20:55:04 +0200

    Updated to match overhaul

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -22,45 +22,84 @@ async function handleLeaderboard(leaderboard) {
     let position=1;
     //CURRENT player rank
     let current_position=1;
-    var playerName=getCookie(COOKIE_PLAYER_NAME);
-
+    let playerName=getCookie(COOKIE_PLAYER_NAME);
+    console.log(playerName);
     for(const entry of leaderboardArray) {
 
         if(playerName===entry['player']){
             current_position=position;
         }
         if(position===1 && playerName!==entry['player']){
-            html += "<tr id='gold'>" +
-                "<td>" +"<span>"+position+"."+"</span>"+ entry['player'] + "</td>" +
-                "<td>" + entry['score'] + "</td>" +
-                "</tr>";
+            html +=
+                "<li id='gold'>"+
+                "<div class='player-name'>"+
+                "<span class='player-rank'>"+
+                position+
+                "."+
+                "</span>"+
+                entry['player']+"</div>"+
+                "<div class='score'>"+entry['score']+
+                "</div>"+
+                "</li>"
+
         }
-        else if(position===2&& playerName!==entry['player']){
-            html += "<tr id='silver'>" +
-                "<td>" +"<span>"+position+"."+"</span>"+ entry['player'] + "</td>" +
-                "<td>" + entry['score'] + "</td>" +
-                "</tr>";
+        else if(position===2&& playerName!==entry['player'] && playerName.length < 20){
+            html +=
+                "<li id='silver'>"+
+                "<div class='player-name'>"+
+                "<span class='player-rank'>"+
+                position+
+                "."+
+                "</span>"+
+                entry['player']+"</div>"+
+                "<div class='score'>"+entry['score']+
+                "</div>"+
+                "</li>"
+
         }
-        else if (position===3&& playerName!==entry['player']){
-            html += "<tr id='bronze'>" +
-                "<td>" + "<span>" + position + "." + "</span>" + entry['player'] + "</td>" +
-                "<td>" + entry['score'] + "</td>" +
-                "</tr>";
+        else if (position===3&& playerName!==entry['player'] && playerName.length < 20){
+            html +=
+                "<li id='bronze'>"+
+                "<div class='player-name'>"+
+                "<span class='player-rank'>"+
+                position+
+                "."+
+                "</span>"+
+                entry['player']+"</div>"+
+                "<div class='score'>"+entry['score']+
+                "</div>"+
+                "</li>"
+
         }
         else if(playerName!==entry['player']){
-            html += "<tr>" +
-                "<td>" + "<span>" + position + "." + "</span>" + entry['player'] + "</td>" +
-                "<td>" + entry['score'] + "</td>" +
-                "</tr>";
+            html +=
+                "<li>"+
+                "<div class='player-name'>"+
+                "<span class='player-rank'>"+
+                position+
+                "."+
+                "</span>"+
+                entry['player']+"</div>"+
+                "<div class='score'>"+entry['score']+
+                "</div>"+
+                "</li>"
         }
         else
-            html += "<tr  id='currentPlayer'>" +
-                "<td>" + "<span>" + position + "." + "</span>" + entry['player'] + "</td>" +
-                "<td>" + entry['score'] + "</td>" +
-                "</tr>";
+            html +=
+                "<li>"+
+                "<div class='player-name'>"+
+                "<span class='player-rank'>"+
+                position+
+                "."+
+                "</span>"+
+                entry['player']+"</div>"+
+                "<div class='score'>"+entry['score']+
+                "</div>"+
+                "</li>"
+
         position++;
     }
-
+    console.log(current_position);
     //function to get score with /th/api/score API call
     async function LeaderboardScore() {
 
@@ -74,6 +113,7 @@ async function handleLeaderboard(leaderboard) {
         "<h2>"+"Your position:  "+current_position+"</h2>";
 
     LeaderboardScore();
-    let leaderboardElement = document.getElementById('output-table'); // table
+
+    let leaderboardElement = document.getElementById('leaderboard'); // table
     leaderboardElement.innerHTML += html; // append generated HTML to existing
 }
