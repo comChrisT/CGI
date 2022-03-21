b0be740345f0262ac4b67dc145b812358714cbdc
VGuowei <93118321+VGuowei@users.noreply.github.com>
2022-03-21 14:02:18 +0200

    comments

--- js/cookies.js
+++ js/cookies.js
@@ -1,10 +1,9 @@
 
-// COOKIES
+// COOKIE NAME
 const COOKIE_CHALLENGE_NAME = "THWebApp-challengeName";
 const COOKIE_PLAYER_NAME = "THWebApp-playerName";
 const COOKIE_SESSION_ID = "THWebApp-sessionID";
 const COOKIE_TOTALL_QUESTIONS = "THWebApp-totallQuestions";
-
 
 // Creates cookie with a given name, value and expire date in days
 function createCookie(cookieName, cookieValue, expireDays) {
@@ -36,6 +35,7 @@ function cookieExists(cookieName) {
     if (getCookie(cookieName) != "") return true; else return false;
 }
 
+// Delete cookie
 function deleteCookie(cookieName){
     document.cookie = cookieName+"=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 }
